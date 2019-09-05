2bebbced77dd0742fb1dfae836c780d73d6287d9
Jaideep Sharma <jaidshar@codeaurora.org>
2019-09-05 16:15:40 +0530

    msm8953-common: Update sound trigger HIDL version to 2.2
    Change-Id: Idb683714c52bfdf17ea709c6f47d312582042190

--- msm8953.mk
+++ msm8953.mk
@@ -60,7 +60,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
