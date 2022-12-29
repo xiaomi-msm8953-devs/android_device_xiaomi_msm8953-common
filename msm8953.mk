2862c64019c10f30f22aa6d2af85cd068983259f
Roberto Sartori <roberto.sartori.android@gmail.com>
2022-12-29 10:30:17 +0100

    msm8953-common: Switch from time_daemon to TimeKeep
      * time_daemon just refuses to work properly on mata.
        Sony's TimeKeep, instead, does the job flawlessly,
        so just ditch the former in favor of the latter.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>
    Change-Id: I54f50ed3d49efc2834d478781b15e6ee428a3062

--- msm8953.mk
+++ msm8953.mk
@@ -275,6 +275,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# TimeKeep
+PRODUCT_PACKAGES += \
+    TimeKeep
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.basic
