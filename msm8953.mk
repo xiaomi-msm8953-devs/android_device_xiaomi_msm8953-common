2c9d327d986ca14fc4c8b551a2f6520e79188249
Giovanni Ricca <giovanniricca@proton.me>
2023-02-19 10:34:32 +0100

    msm8953-common: Commonize bootanimation flags
    * And make them overideble overridable
    
    Change-Id: Ic31ba986d24df1a29aafcab150e873740a1a3e82

--- msm8953.mk
+++ msm8953.mk
@@ -21,6 +21,10 @@ OVERRIDE_PRODUCT_COMPRESSED_APEX := false
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG ?= xxhdpi
 
+# Boot animation
+TARGET_SCREEN_HEIGHT ?= 1920
+TARGET_SCREEN_WIDTH ?= 1080
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
