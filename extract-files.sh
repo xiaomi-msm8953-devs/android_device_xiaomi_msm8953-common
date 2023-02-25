84846914f244ac1d22218a052e7e5d31d83c8a3a
Giovanni Ricca <giovanniricca@proton.me>
2023-02-25 11:09:29 +0100

    msm8953-common: Commonize qcril blobs
    This reverts commit 9a0c0303d5ab9a2fd1ce6f1a7726f2638304c7db.
    
    Change-Id: Ie486479236154d91d8857baa59c5ef1ceacfd1c3

--- extract-files.sh
+++ extract-files.sh
@@ -89,6 +89,9 @@ function blob_fixup() {
         vendor/lib/mediadrm/libwvdrmengine.so|vendor/lib64/mediadrm/libwvdrmengine.so)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
             ;;
+        vendor/lib64/libril-qc-hal-qmi.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
+            ;;
         vendor/lib64/libsettings.so)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
             ;;
