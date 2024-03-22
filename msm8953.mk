c21776385c607f3d82e2dd9bfa3c8b011fd45a6c
Aaron Kling <webgeek1234@gmail.com>
2024-03-22 08:05:53 -0500

    msm8953-common: Uprev usb hal to 1.3
    Change-Id: I5d34c71663b31fe03b0d28f8c76e5a4f8cd2385c

--- msm8953.mk
+++ msm8953.mk
@@ -356,7 +356,7 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service.basic
 
 # VNDK
 PRODUCT_COPY_FILES += \
