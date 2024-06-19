09b5acffda09c3116443c9b868b15a6dba60d31a
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-19 11:26:40 +0800

    msm8953-common: Build AOSP Radio HIDL->AIDL Wrapper
    Change-Id: Iaeb8d37f4a7f02e6e812bc4da1b413c9a6493f8f

--- msm8953.mk
+++ msm8953.mk
@@ -225,6 +225,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor # Needed by CNE app
 
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.radio-service.compat
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.zram \
