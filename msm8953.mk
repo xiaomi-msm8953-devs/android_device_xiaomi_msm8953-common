9326e51bb98563ece946040a3f0c19e349a4d11c
LuK1337 <priv.luk@gmail.com>
2025-01-17 17:06:51 +0100

    msm8953-common: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de

--- msm8953.mk
+++ msm8953.mk
@@ -184,7 +184,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light-service.lineage
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
