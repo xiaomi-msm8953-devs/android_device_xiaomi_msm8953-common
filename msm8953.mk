b3a794833e16cdf3ed2df567445d0c348dd8b2c7
Jan Altensen <info@stricted.net>
2025-02-07 21:07:11 +0100

    msm8953-common: move to new fm vendor jni variable
    Change-Id: I48a4c0005a0761ca4c6ff574fca0153d2dbf54c9

--- msm8953.mk
+++ msm8953.mk
@@ -139,6 +139,7 @@ PRODUCT_PACKAGES += \
     FMRadio
 
 $(call soong_config_set_bool,libfmjni,no_fm_firmware,true)
+$(call soong_config_set,libfmjni,vendor,qcom)
 
 # GPS / Location
 PRODUCT_PACKAGES += \
