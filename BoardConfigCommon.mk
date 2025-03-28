e0f2c22435b7b76b6bbb700d8512bef1e797bf9d
Giovanni Ricca <giovanniricca@proton.me>
2025-03-28 21:54:20 +0100

    msm8953-common: Temporarily downgrade kernel clang version
    19.0.1 doesn't boot for us. Let's keep using the old one for now.
    
    Change-Id: I6693b36674539c20870be49d564b652a93478de1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,6 +29,7 @@ BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
 TARGET_KERNEL_CONFIG := msm8953-perf_defconfig xiaomi/xiaomi.config
+TARGET_KERNEL_CLANG_VERSION := r530567
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
